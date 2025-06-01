5bd8cacf44a5366a900e52b352417a00b1c788f3
ksarkar-devops <kaushik89sarkar@gmail.com>
2025-06-01 21:03:44 +0530

    Update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -28,13 +28,7 @@ provider "azurerm" {
 
 
 
-resource "azurerm_storage_account" "storage" {
-  name                     = "funcappstore${random_string.suffix.result}"
-  resource_group_name      = azurerm_resource_group.rg.name
-  location                 = azurerm_resource_group.rg.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-}
+
 
 resource "azurerm_app_service_plan" "plan" {
   name                = "funcapp-plan"
