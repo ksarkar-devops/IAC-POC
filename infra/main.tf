8bb17af502459a0821d9531593246e584b2538b3
ksarkar-devops <kaushik89sarkar@gmail.com>
2025-06-01 10:48:52 +0530

    Update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -1,3 +1,9 @@
+variable "client_id" {}
+variable "client_secret" {}
+variable "tenant_id" {}
+variable "subscription_id" {}
+
+
 # Configure the Azure provider
 terraform {
   required_providers {
@@ -12,6 +18,11 @@ terraform {
 
 provider "azurerm" {
   features {}
+
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  tenant_id       = var.tenant_id
+  subscription_id = var.subscription_id
 }
 
 resource "random_string" "suffix" {
