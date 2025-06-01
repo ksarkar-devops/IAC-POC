6d6fba242c5e61633f5b69ab4f14317d9f301c2b
ksarkar-devops <kaushik89sarkar@gmail.com>
2025-06-01 09:48:22 +0530

    Create main.tf

---
+++ infra/main.tf
@@ -0,0 +1,51 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "random_string" "suffix" {
+  length  = 6
+  upper   = false
+  special = false
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "funcapp-rg"
+  location = "East US"
+}
+
+resource "azurerm_storage_account" "storage" {
+  name                     = "funcappstore${random_string.suffix.result}"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_app_service_plan" "plan" {
+  name                = "funcapp-plan"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  kind                = "FunctionApp"
+  reserved            = false  # false for Windows, true for Linux
+
+  sku {
+    tier = "Dynamic"
+    size = "Y1"
+  }
+}
+
+resource "azurerm_function_app" "funcapp" {
+  name                       = "dotnet-func-${random_string.suffix.result}"
+  location                   = azurerm_resource_group.rg.location
+  resource_group_name        = azurerm_resource_group.rg.name
+  app_service_plan_id        = azurerm_app_service_plan.plan.id
+  storage_account_name       = azurerm_storage_account.storage.name
+  storage_account_access_key = azurerm_storage_account.storage.primary_access_key
+  os_type                    = "windows"
+  version                    = "~4" # Azure Functions v4 supports .NET 6 and 8
+
+  app_settings = {
+    FUNCTIONS_WORKER_RUNTIME = "dotnet"
+    WEBSITE_RUN_FROM_PACKAGE = "1"  # Will need to upload a zip package
+  }
+}
