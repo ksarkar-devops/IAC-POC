8e6e72f200a41afe0f39d32a4ea5448a5f68f9ad
ksarkar-devops <kaushik89sarkar@gmail.com>
2025-06-01 21:05:31 +0530

    Update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -25,24 +25,3 @@ provider "azurerm" {
   tenant_id       = var.tenant_id
   subscription_id = var.subscription_id
 }
-
-
-
-
-
-
-
-resource "azurerm_function_app" "funcapp" {
-  name                       = "dotnet-func-${random_string.suffix.result}"
-  location                   = azurerm_resource_group.rg.location
-  resource_group_name        = azurerm_resource_group.rg.name
-  app_service_plan_id        = azurerm_app_service_plan.plan.id
-  storage_account_name       = azurerm_storage_account.storage.name
-  storage_account_access_key = azurerm_storage_account.storage.primary_access_key
-  version                    = "~4" # Azure Functions v4 supports .NET 6 and 8
-
-  app_settings = {
-    FUNCTIONS_WORKER_RUNTIME = "dotnet"
-    WEBSITE_RUN_FROM_PACKAGE = "1"  # Will need to upload a zip package
-  }
-}
