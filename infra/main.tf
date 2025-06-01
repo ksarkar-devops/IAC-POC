95b89d53de484f3b641a22590f212cbac67610bd
ksarkar-devops <kaushik89sarkar@gmail.com>
2025-06-01 21:04:35 +0530

    Update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -30,18 +30,7 @@ provider "azurerm" {
 
 
 
-resource "azurerm_app_service_plan" "plan" {
-  name                = "funcapp-plan"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  kind                = "FunctionApp"
-  reserved            = false  # false for Windows, true for Linux
 
-  sku {
-    tier = "Dynamic"
-    size = "Y1"
-  }
-}
 
 resource "azurerm_function_app" "funcapp" {
   name                       = "dotnet-func-${random_string.suffix.result}"
